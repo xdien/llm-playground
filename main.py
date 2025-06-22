016a3f2ac7c7bb843e9f4a52906616f85ed25ed5
Xuan Dien <xdienw@gmail.com>
2025-06-22 08:00:40 +0700

    fix: use english comments

---
+++ main.py
@@ -0,0 +1,11 @@
+def hello_world():
+    # This is an English comment.
+    print("Hello, World!")
+
+def good_bye():
+    # This is also an English comment.
+    print("Good bye!")
+
+if __name__ == "__main__":
+    hello_world()
+    good_bye() 
